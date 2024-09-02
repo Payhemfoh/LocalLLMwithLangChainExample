ac0a9fcd9026e68ed16a648222bbededbb15fe9f
Yafumin Foh <56668252+Payhemfoh@users.noreply.github.com>
2024-09-02 13:36:45 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+from langchain_ollama import ChatOllama
+from langchain.agents import tool
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.agents.format_scratchpad.openai_tools import (format_to_openai_tool_messages)
+from langchain.agents import AgentExecutor
+from langchain.agents.output_parsers.openai_tools import OpenAIToolsAgentOutputParser
+
+llm = ChatOllama(model="llama3.1", temperature=0, verbose=True)
+
+@tool
+def get_word_length(word:str) -> int:
+    """Returns the length of a word."""
+    return len(word)
+
+tools = [get_word_length]
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are very powerful assistant",
+        ),
+        ("user", "{input}"),
+        MessagesPlaceholder(variable_name="agent_scratchpad"),
+    ]
+)
+
+llm_with_tools = llm.bind_tools(tools)
+
+agent = (
+    {
+        "input": lambda x: x["input"],
+        "agent_scratchpad": lambda x: format_to_openai_tool_messages(
+            x["intermediate_steps"]
+        ),
+    }
+    | prompt
+    | llm_with_tools
+    | OpenAIToolsAgentOutputParser()
+)
+
+#Create an agent executor by passing in the agent and tools
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+result = agent_executor.invoke({"input": "List down ten adjectives and number of letters for each word and sum up the number of letters for all the given adjectives."})
+
+if result:
+    print(f"[Output] --> {result['output']}\n")
+else:
+    print("[Output] --> null\n")
